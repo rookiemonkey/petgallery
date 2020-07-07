7c7cbc7aa65140e89ab84663e7dfb0ceb6291d41
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-07 15:28:03 +0800

    fixed home mobile view

--- src/components/mini/DogCard.js
+++ src/components/mini/DogCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DogCard = props => (
-    < div className='col-3 d-flex flex-column  align-items-center' >
+    <div className='col-sm-12 col-md-4 col-lg-3 d-flex flex-column align-items-center' >
         <Link to={`/${props.name}/${props.ind}`} >
             <img
                 src={props.img}
