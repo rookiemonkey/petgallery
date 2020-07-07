e24e86943033caab2f6f35161059ae3852da1921
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-07 09:08:26 +0800

    initial commit/setup done

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>THis is home</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Dog from './components/Dog';
+import './index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/:dog' component={Dog} />
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
