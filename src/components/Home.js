8d7a1f3aa2e552f60bd040c69769085796a0e9e9
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-07 10:23:46 +0800

    added fetch random names

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,25 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dogs: new Array(10)
+      dogs: new Array(10),
+      names: []
     }
   }
 
   componentDidMount() {
     fetch('https://dog.ceo/api/breeds/image/random/10')
       .then(r => { return r.json() })
-      .then(d => { this.setState({ dogs: d.message }) })
+      .then(d => {
+        this.setState({ ...this.state, dogs: d.message }, () => {
+          fetch('https://randomuser.me/api/?results=10')
+            .then(r => { return r.json() })
+            .then(d => {
+              const names = d.results.map(o => { return o.name.first })
+              this.setState({ ...this.state, names: names }, () => { console.log(this.state) })
+            })
+            .catch(e => { console.error('Something went upon fetching the names:', e.message) })
+        })
+      })
       .catch(e => { console.error('Something went upon fetching the dogs:', e.message) })
   }
 
