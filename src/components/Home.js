22dea96d179fcc65e4f4e0970b6333ee636c1393
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-20 17:44:48 +0800

    tweaked markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,11 +15,20 @@ class Home extends Component {
 
   componentDidMount() {
     if (!sessionStorage.Dogs && !sessionStorage.Names) {
-      Promise.all([getDogs(12), getNames(12)])
-        .then(d => { this.setState({ dogs: d[0], names: d[1] }) })
-        .catch(e => { console.error(`Something went wrong upon fetching data ${e.message}`) })
+      const numberOfDogs = 12;
+      const numberOfNames = 12;
+      Promise.all([getDogs(numberOfDogs), getNames(numberOfNames)])
+        .then(data => {
+          this.setState({ dogs: data[0], names: data[1] })
+        })
+        .catch(error => {
+          console.error(`Something went wrong upon fetching data ${error.message}`)
+        })
     } else {
-      this.setState({ dogs: JSON.parse(sessionStorage.Dogs), names: JSON.parse(sessionStorage.Names) })
+      this.setState({
+        dogs: JSON.parse(sessionStorage.Dogs),
+        names: JSON.parse(sessionStorage.Names)
+      })
     }
   }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './components/Home';
 import Dog from './components/Dog';
-import './index.scss';
+import './assets/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
